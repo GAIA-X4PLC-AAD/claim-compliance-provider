26ab63f5e1d7ff0611bb190915260682e79b094f
schuberr <robert.schubert@msg.group>
2024-06-23 16:29:28 +0200

    solved linting errors, add readme and util class.

---
+++ claim-compliance-provider-api/src/main/java/com/msg/ccp/util/VpVcUtil.java
@@ -0,0 +1,61 @@
+package com.msg.ccp.util;
+
+import com.danubetech.verifiablecredentials.VerifiableCredential;
+import com.danubetech.verifiablecredentials.VerifiablePresentation;
+
+import java.util.Map;
+
+/**
+ * util class for VP and VC.
+ * VerifiablePresentation and VerifiableCredential do only know id and type, but not @id and @type (from the json ld spec).
+ */
+public class VpVcUtil {
+
+    public static String getType(final VerifiablePresentation verifiablePresentation) {
+        final Map<String, Object> vpMap = verifiablePresentation.toMap();
+        return obtainType(vpMap);
+    }
+
+    public static String getType(final VerifiableCredential verifiableCredential) {
+        final Map<String, Object> vpMap = verifiableCredential.toMap();
+        return obtainType(vpMap);
+    }
+
+    public static String getType(final Map<String, Object> map) {
+        return obtainType(map);
+    }
+
+    public static String getId(final VerifiablePresentation verifiablePresentation) {
+        final Map<String, Object> vpMap = verifiablePresentation.toMap();
+        return obtainId(vpMap);
+    }
+
+    public static String getId(final VerifiableCredential verifiableCredential) {
+        final Map<String, Object> vpMap = verifiableCredential.toMap();
+        return obtainId(vpMap);
+    }
+
+    public static String getId(final Map<String, Object> map) {
+        return obtainId(map);
+    }
+
+    private static String obtainType(final Map<String, Object> map) {
+        if (map.containsKey("type")) {
+            return map.get("type").toString();
+        } else if (map.containsKey("@type")) {
+            return map.get("@type").toString();
+        } else return null;
+    }
+
+    private static String obtainId(final Map<String, Object> map) {
+        if (map.containsKey("id")) {
+            return map.get("id").toString();
+        } else if (map.containsKey("@id")) {
+            return map.get("@id").toString();
+        } else return null;
+    }
+
+    private VpVcUtil() {
+        super();
+    }
+}
---
+++ claim-compliance-provider-claimcreator/src/main/resources/README.md
@@ -0,0 +1,12 @@
+# Update example claims after changing the json files.
+
+Once one of the json file is changed, the example payloads should be updated in class `SendClaimsPayload.LIST_OF_CLAIMS`.
++ Perform build
++ Search for "Claims: " in the log output
++ Copy the claims and paste them in the class `SendClaimsPayload.LIST_OF_CLAIMS` (without surrounding brackets `[` and `]`)
++ Format json (e. g. with Visual Studio Code: `Alt` + `Shift` + `F`)
++ Copy and past into `LIST_OF_CLAIMS` 
+
+`LIST_OF_CLAIMS` is then embedded automatically into the `SendClaimsPayload.EXAMPLE_PAYLOAD` and `GenerateClaimsPayload.EXAMPLE_RESPONSE` classes.
+
+> **!!!** Dont forget to update the ID of the domain specific class in `SendClaimsPayload.EXAMPLE_PAYLOAD`! **IT MUST MATCH THE ID OF THE DataResource !!!** 
