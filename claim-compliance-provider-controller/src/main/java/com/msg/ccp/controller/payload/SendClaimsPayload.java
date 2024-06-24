9abf11bc40a9c5ec068d10692bf903170189467b
schuberr <robert.schubert@msg.group>
2024-06-24 16:36:13 +0200

    updated payloads (removed @ when @id was linked)

--- claim-compliance-provider-controller/src/main/java/com/msg/ccp/controller/payload/SendClaimsPayload.java
+++ claim-compliance-provider-controller/src/main/java/com/msg/ccp/controller/payload/SendClaimsPayload.java
@@ -18,158 +18,162 @@ public record SendClaimsPayload(@NotNull Set<Map<String, Object>> claims,
      */
     public static final String LIST_OF_CLAIMS = """
             {
-               "@context": {
-                 "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
-                 "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               },
-               "@id": "https://www.gaia-x4plcaad.info/claims/virtual-resource/1081c710-ae91-4f77-b0be-ee0552eb6cb6",
-               "@type": "gx:InstantiatedVirtualResource",
-               "gx:maintainedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:hostedOn": {
-                 "@id": "https://www.gaia-x4plcaad.info/claims/physical-resource/1081c710-ae91-4f77-b0be-ee0552eb6cb6"
-               },
-               "gx:instanceOf": {
-                 "@id": "https://www.gaia-x4plcaad.info/claims/data-resource/1081c710-ae91-4f77-b0be-ee0552eb6cb6"
-               },
-               "gx:tenantOwnedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:serviceAccessPoint": {
-                 "@id": "https://www.gaia-x4plcaad.info/claims/service-access-point/1081c710-ae91-4f77-b0be-ee0552eb6cb6"
-               }
-             },
-             {
-               "@context": {
-                 "xsd": "http://www.w3.org/2001/XMLSchema#",
-                 "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
-                 "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               },
-               "@id": "https://www.gaia-x4plcaad.info/claims/physical-resource/1081c710-ae91-4f77-b0be-ee0552eb6cb6",
-               "@type": "gx:PhysicalResource",
-               "gx:maintainedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:ownedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:manufacturedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:location": {
-                 "@value": "DE",
-                 "@type": "xsd:string"
-               },
-               "gx:name": {
-                 "@value": "Generated PhysicalResource",
-                 "@type": "xsd:string"
-               }
-             },
-             {
-               "@context": {
-                 "xsd": "http://www.w3.org/2001/XMLSchema#",
-                 "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
-                 "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               },
-               "@id": "https://www.gaia-x4plcaad.info/claims/service-offering/1081c710-ae91-4f77-b0be-ee0552eb6cb6",
-               "@type": "gx:ServiceOffering",
-               "gx:dataAccountExport": {
-                 "gx:requestType": "API",
-                 "gx:formatType": {
-                   "@value": "application/json",
-                   "@type": "xsd:string"
-                 },
-                 "gx:accessType": "digital"
-               },
-               "gx:termsAndConditions": {
-                 "gx:hash": {
-                   "@value": "d056db972da41899ed89cd3f93b8ccd59fc9314e2904af3455ce13d76a2ed99b",
-                   "@type": "xsd:string"
-                 },
-                 "gx:URL": {
-                   "@value": "https://www.msg.group/sotermsandconditions",
-                   "@type": "xsd:string"
-                 }
-               },
-               "gx:policy": {
-                 "@value": "package access_control default allow = false  allow { input.method == \\"GET\\" input.path == /public } allow { input.method == \\"POST\\"     input.path == \\"/private\\"     input.user.role == \\"admin\\" }",
-                 "@type": "xsd:string"
-               },
-               "gx:providedBy": {
-                 "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               }
-             },
-             {
-               "@context": {
-                 "xsd": "http://www.w3.org/2001/XMLSchema#",
-                 "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
-                 "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               },
-               "@id": "https://www.gaia-x4plcaad.info/claims/data-resource/1081c710-ae91-4f77-b0be-ee0552eb6cb6",
-               "@type": "gx:DataResource",
-               "gx:copyrightOwnedBy": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json",
-               "gx:license": "Apache-2.0",
-               "gx:obsoleteDateTime": {
-                 "@value": "2025-03-23T14:57:15",
-                 "@type": "xsd:dateTime"
-               },
-               "gx:exposedThrough": {
-                 "@id": "https://www.gaia-x4plcaad.info/claims/service-offering/1081c710-ae91-4f77-b0be-ee0552eb6cb6"
-               },
-               "gx:expirationDateTime": {
-                 "@value": "2025-03-23T14:57:15",
-                 "@type": "xsd:dateTime"
-               },
-               "gx:containsPII": false,
-               "gx:policy": {
-                 "@value": "package access_control  default allow = false  allow {     input.method == \\"GET\\"     input.path == \\"/public\\" }  allow {     input.method == \\"POST\\"     input.path == \\"/private\\"     input.user.role == \\"admin\\" }",
-                 "@type": "xsd:string"
-               },
-               "gx:producedBy": {
-                 "@id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
-               },
-               "gx:name": {
-                 "@value": "Generated Data Resource",
-                 "@type": "xsd:string"
-               },
-               "gx:description": {
-                 "@value": "Generated description.",
-                 "@type": "xsd:string"
-               }
-             },
-             {
-               "@context": {
-                 "xsd": "http://www.w3.org/2001/XMLSchema#",
-                 "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#"
-               },
-               "@id": "https://www.gaia-x4plcaad.info/claims/service-access-point/1081c710-ae91-4f77-b0be-ee0552eb6cb6",
-               "@type": "gx:ServiceAccessPoint",
-               "gx:name": {
-                 "@value": "Provider EDC",
-                 "@type": "xsd:string"
-               },
-               "gx:host": {
-                 "@value": "edcdb-pr.gxfs.gx4fm.org/",
-                 "@type": "xsd:string"
-               },
-               "gx:protocol": {
-                 "@value": "https",
-                 "@type": "xsd:string"
-               },
-               "gx:version": {
-                 "@value": "0.2.1",
-                 "@type": "xsd:string"
-               },
-               "gx:port": {
-                 "@value": "443",
-                 "@type": "xsd:string"
-               },
-               "gx:openAPI": {
-                 "@value": "https://app.swaggerhub.com/apis/eclipse-edc-bot/management-api/0.2.1",
-                 "@type": "xsd:string"
-               }
-             }
+                "@context": {
+                  "xsd": "http://www.w3.org/2001/XMLSchema#",
+                  "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#"
+                },
+                "@id": "https://www.gaia-x4plcaad.info/claims/service-access-point/25929d5f-a577-475c-aab8-aa05c65e8d1c",
+                "@type": "gx:ServiceAccessPoint",
+                "gx:name": {
+                  "@value": "Provider EDC",
+                  "@type": "xsd:string"
+                },
+                "gx:host": {
+                  "@value": "edcdb-pr.gxfs.gx4fm.org/",
+                  "@type": "xsd:string"
+                },
+                "gx:protocol": {
+                  "@value": "https",
+                  "@type": "xsd:string"
+                },
+                "gx:version": {
+                  "@value": "0.2.1",
+                  "@type": "xsd:string"
+                },
+                "gx:port": {
+                  "@value": "443",
+                  "@type": "xsd:string"
+                },
+                "gx:openAPI": {
+                  "@value": "https://app.swaggerhub.com/apis/eclipse-edc-bot/management-api/0.2.1",
+                  "@type": "xsd:string"
+                }
+              },
+              {
+                "@context": {
+                  "xsd": "http://www.w3.org/2001/XMLSchema#",
+                  "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
+                  "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                },
+                "@id": "https://www.gaia-x4plcaad.info/claims/service-offering/25929d5f-a577-475c-aab8-aa05c65e8d1c",
+                "@type": "gx:ServiceOffering",
+                "gx:dataAccountExport": {
+                  "gx:requestType": "API",
+                  "gx:formatType": {
+                    "@value": "application/json",
+                    "@type": "xsd:string"
+                  },
+                  "gx:accessType": "digital"
+                },
+                "gx:termsAndConditions": {
+                  "gx:hash": {
+                    "@value": "d056db972da41899ed89cd3f93b8ccd59fc9314e2904af3455ce13d76a2ed99b",
+                    "@type": "xsd:string"
+                  },
+                  "gx:URL": {
+                    "@value": "https://www.msg.group/sotermsandconditions",
+                    "@type": "xsd:string"
+                  }
+                },
+                "gx:policy": {
+                  "@value": "package access_control default allow = false  allow { input.method == \\"GET\\" input.path == /public } allow { input.method == \\"POST\\"     input.path == \\"/private\\"     input.user.role == \\"admin\\" }",
+                  "@type": "xsd:string"
+                },
+                "gx:providedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                }
+              },
+              {
+                "@context": {
+                  "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
+                  "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                },
+                "@id": "https://www.gaia-x4plcaad.info/claims/virtual-resource/25929d5f-a577-475c-aab8-aa05c65e8d1c",
+                "@type": "gx:InstantiatedVirtualResource",
+                "gx:maintainedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:hostedOn": {
+                  "id": "https://www.gaia-x4plcaad.info/claims/physical-resource/25929d5f-a577-475c-aab8-aa05c65e8d1c"
+                },
+                "gx:instanceOf": {
+                  "id": "https://www.gaia-x4plcaad.info/claims/data-resource/25929d5f-a577-475c-aab8-aa05c65e8d1c"
+                },
+                "gx:tenantOwnedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:serviceAccessPoint": {
+                  "id": "https://www.gaia-x4plcaad.info/claims/service-access-point/25929d5f-a577-475c-aab8-aa05c65e8d1c"
+                }
+              },
+              {
+                "@context": {
+                  "xsd": "http://www.w3.org/2001/XMLSchema#",
+                  "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
+                  "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                },
+                "@id": "https://www.gaia-x4plcaad.info/claims/physical-resource/25929d5f-a577-475c-aab8-aa05c65e8d1c",
+                "@type": "gx:PhysicalResource",
+                "gx:maintainedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:ownedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:manufacturedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:location": {
+                  "@value": "DE",
+                  "@type": "xsd:string"
+                },
+                "gx:name": {
+                  "@value": "Generated PhysicalResource",
+                  "@type": "xsd:string"
+                },
+                "gx:description": {
+                  "@value": "Generated PhysicalResource Description.",
+                  "@type": "xsd:string"
+                }
+              },
+              {
+                "@context": {
+                  "xsd": "http://www.w3.org/2001/XMLSchema#",
+                  "gx": "https://registry.lab.gaia-x.eu/development/api/trusted-shape-registry/v1/shapes/jsonld/trustframework#",
+                  "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                },
+                "@id": "https://www.gaia-x4plcaad.info/claims/data-resource/25929d5f-a577-475c-aab8-aa05c65e8d1c",
+                "@type": "gx:DataResource",
+                "gx:copyrightOwnedBy": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json",
+                "gx:license": "Apache-2.0",
+                "gx:obsoleteDateTime": {
+                  "@value": "2025-03-24T14:32:37",
+                  "@type": "xsd:dateTime"
+                },
+                "gx:exposedThrough": {
+                  "id": "https://www.gaia-x4plcaad.info/claims/service-offering/25929d5f-a577-475c-aab8-aa05c65e8d1c"
+                },
+                "gx:expirationDateTime": {
+                  "@value": "2025-03-24T14:32:37",
+                  "@type": "xsd:dateTime"
+                },
+                "gx:containsPII": false,
+                "gx:policy": {
+                  "@value": "package access_control  default allow = false  allow {     input.method == \\"GET\\"     input.path == \\"/public\\" }  allow {     input.method == \\"POST\\"     input.path == \\"/private\\"     input.user.role == \\"admin\\" }",
+                  "@type": "xsd:string"
+                },
+                "gx:producedBy": {
+                  "id": "https://participant.gxfs.gx4fm.org/msg-systems-ag/legalParticipant.json"
+                },
+                "gx:name": {
+                  "@value": "Generated Data Resource",
+                  "@type": "xsd:string"
+                },
+                "gx:description": {
+                  "@value": "Generated description.",
+                  "@type": "xsd:string"
+                }
+              }
             """;
     public static final String EXAMPLE_PAYLOAD = """
             {
