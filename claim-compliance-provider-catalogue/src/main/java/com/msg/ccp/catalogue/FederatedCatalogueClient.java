a027068132ac93e07cdf09f41d97e9ea3068ddb7
schuberr <robert.schubert@msg.group>
2024-06-24 13:43:55 +0200

    added valid payload to federated catalogue test, solved linting messages

--- claim-compliance-provider-catalogue/src/main/java/com/msg/ccp/catalogue/FederatedCatalogueClient.java
+++ claim-compliance-provider-catalogue/src/main/java/com/msg/ccp/catalogue/FederatedCatalogueClient.java
@@ -1,11 +1,10 @@
 package com.msg.ccp.catalogue;
 
+import com.danubetech.verifiablecredentials.VerifiablePresentation;
 import com.msg.ccp.interfaces.catalogue.FederatedCatalogueResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-import java.util.Map;
 
 @ApplicationScoped
 @RegisterRestClient
@@ -14,5 +13,5 @@ public interface FederatedCatalogueClient {
     @Path("/verification")
     @Produces("application/json")
     @Consumes("application/json")
-    FederatedCatalogueResponse verification(@HeaderParam("Authorization") String accessToken, final Map<String, Object> verifiablePresentation);
+    FederatedCatalogueResponse verification(@HeaderParam("Authorization") String accessToken, final VerifiablePresentation verifiablePresentation);
 }
